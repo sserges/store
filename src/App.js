14cfc4c3e9f13973abe90580db31f04ee0f722a5
sserges <agounkeserge@gmail.com>
2022-11-25 18:56:37 -0400

    Implementing React Router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import { Container } from 'react-bootstrap'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import NavbarComponent from './components/Navbar'
 
 function App() {
   return (
-    <>
+    <Container>
       <NavbarComponent />
-    </>
+    </Container>
   )
 }
 
