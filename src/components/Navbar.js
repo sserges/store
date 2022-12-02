2ceed05ecac50afdedc853121a2594c9b163140a
sserges <agounkeserge@gmail.com>
2022-12-02 18:25:43 -0400

    Adding cart functionality to navbar (number of items)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,19 @@
-import { Button, Container, Navbar, Modal } from 'react-bootstrap'
-import { useState } from 'react'
+import { Button, Navbar, Modal } from 'react-bootstrap'
+import { useState, useContext } from 'react'
+
+import { CartContext } from '../CartContext'
 
 function NavbarComponent() {
+  const cart = useContext(CartContext)
+
   const [show, setShow] = useState(false)
   const handleClose = () => setShow(false)
   const handleShow = () => setShow(true)
+
+  const productsCount = cart.items.reduce(
+    (sum, product) => sum + product.quantity,
+    0
+  )
 
   return (
     <>
@@ -12,7 +21,7 @@ function NavbarComponent() {
         <Navbar.Brand href='/'>Ecommerce Store</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className='justify-content-end'>
-          <Button onClick={handleShow}>Cart 0 Items</Button>
+          <Button onClick={handleShow}>Cart ({productsCount} Items)</Button>
         </Navbar.Collapse>
       </Navbar>
 
